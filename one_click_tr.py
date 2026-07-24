dfe71903cd29ed1d007a9cf8fe3e787ffa5b2705
gaideaa123 <gaideaa123@github.com>
2026-07-24 14:34:51 +0300

    clean: remove extra files and fix privacy dropdown click issue

--- one_click_tr.py
+++
@@ -1,7 +0,0 @@
-import app_tr
-import session_gui
-
-session_gui.install(app_tr.TurkceAnaPencere)
-
-if __name__ == "__main__":
-    raise SystemExit(app_tr.main())
--- run_one_click.py
+++
@@ -1,4 +0,0 @@
-from app_tr import main
-
-if __name__ == "__main__":
-    raise SystemExit(main())
